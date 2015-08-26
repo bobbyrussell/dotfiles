2e963348db82aa8b6f14d592e775b458d9147c09
Bobby Russell <bobbyrussell@gmail.com>
2015-08-25 23:46:20 -0400

    Update install.sh to handle ~/.vim
    install.sh now backs up ~/.vim and links it with the repo's
    vim/autoload and vim/bundle

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 # Backs up the dotfiles in $HOME, copies all the dirs in $RC_FILE_DIRS to
 # $DOTFILE_DIR, and then force symlinks all of the files in $DOTFILE_DIR
-# to corresponding dotfiles in $HOME
+# to corresponding dotfiles in $HOME. The $VIM_DIR will also be overwritten
+# with the contents of vim/autoload and vim/bundle.
 
-DOTFILE_DIR="${PWD}"
 DOTFILE_BACKUP_DIR="${HOME}/.dotfile.backups"
+DOTFILE_DIR="${PWD}"
 RC_FILE_DIRS="emacs vim mux sh git"
-
-if [ ! -d ${DOTFILE_DIR} ]
-then
-    mkdir "${DOTFILE_DIR}"
-fi
+VIM_DIR="${HOME}/.vim"
+AUTOLOAD="${VIM_DIR}/autoload"
+BUNDLE="${VIM_DIR}/bundle"
 
 if [ ! -d "${DOTFILE_BACKUP_DIR}" ]
 then
@@ -35,4 +34,36 @@ for dir in ${RC_FILE_DIRS}; do
     done
 done
 
-echo "Finished installing the dotfiles. You can manage them from ${DOTFILE_DIR}"
+if [ -d "${VIM_DIR}" ]
+then
+    echo "Syncing ${VIM_DIR} to ${DOTFILE_BACKUP_DIR}"
+    VIM_BACKUP="${DOTFILE_BACKUP_DIR}/.vim"
+    if [ -d  "${VIM_BACKUP}" ]
+    then
+        rm -rf "${VIM_BACKUP}"
+    fi
+    cp -r "${VIM_DIR}" "${VIM_BACKUP}"
+    rm -rf "${VIM_DIR}"
+fi
+echo "Creating new .vim/ at ${HOME}"
+mkdir "${VIM_DIR}"
+
+echo "Linking vim/autoload and vim/bundle to ${VIM_DIR}"
+ln -s "${DOTFILE_DIR}/vim/autoload" "${AUTOLOAD}"
+ln -s "${DOTFILE_DIR}/vim/bundle" "${BUNDLE}"
+
+echo "Syncing plugins..."
+git submodule init
+git submodule update
+echo "Plugins synced"
+
+echo
+echo "==========================================="
+echo "Finished installing dotfiles"
+echo
+echo "Manage your dotfiles from:"
+echo -e '\t' "${DOTFILE_DIR}"
+echo
+echo "Your backups are located at:"
+echo -e '\t' "${DOTFILE_BACKUP_DIR}"
+echo "==========================================="
